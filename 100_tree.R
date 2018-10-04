b3f3c8fa96c2d737503b670f679efcc90a69d495
dave <kika.davies@gmail.com>
2018-10-04 11:20:26 +0300

    Update 100_tree.R
    comment out lines for use in generic site

--- 100_tree.R
+++ 100_tree.R
@@ -1,6 +1,6 @@
 require(ape)
 getwd()
-read.FASTA('~/mito_Seq.fasta') -> ape
-sub("\\..*","",names(ape)) -> accesions
-tree = rmtree(N=100,n=7, rooted = F, tip.label = accesions,br=0.1)
+# read.FASTA('~/mito_Seq.fasta') -> ape
+#sub("\\..*","",names(ape)) -> accesions
+tree = rmtree(N=100,n=7, rooted = F, tip.label = NULL,br=0.1)
 write.tree(phy=tree, file= "hundred_trees.nh")
