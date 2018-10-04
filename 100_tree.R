5b41472c6d978f9e831a883545ede637dc679cf1
dave <kika.davies@gmail.com>
2018-10-04 12:39:33 +0300

    Update 100_tree.R

--- 100_tree.R
+++ 100_tree.R
@@ -1,6 +1,6 @@
 require(ape)
-getwd()
+#getwd()
 # read.FASTA('~/mito_Seq.fasta') -> ape
 #sub("\\..*","",names(ape)) -> accesions
 tree = rmtree(N=100,n=7, rooted = F, tip.label = NULL,br=0.1)
-write.tree(phy=tree, file= "hundred_trees.nh")
+write.tree(phy=tree, file= "")# ./hundred_trees.nh
